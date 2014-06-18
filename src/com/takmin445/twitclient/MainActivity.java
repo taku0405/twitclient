32dd4acbfa992a177c6693d0c31d8ac52478a053
taku <taku_405_2zigen@live.jp>
2014-06-18 09:48:51 +0900

    06/18-0948

--- src/com/takmin445/twitclient/MainActivity.java
+++ src/com/takmin445/twitclient/MainActivity.java
@@ -1,30 +1,95 @@
 package com.takmin445.twitclient;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import twitter4j.ResponseList;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.app.Activity;
+import android.app.ListActivity;
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
 import android.os.Build;
 
-public class MainActivity extends Activity {
+public class MainActivity extends ListActivity {
 
+	private TweetAdapter mAdapter;
+	private Twitter mTwitter;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		//setContentView(R.layout.activity_main);
 
 		if(!TwitUtils.hasAccessToken(this)){
 			Intent intent = new Intent(this, TwitterOAuthActivity.class);
 			startActivity(intent);
 			finish();
+		}else{
+			mAdapter = new TweetAdapter(this);
+			setListAdapter(mAdapter);
+			
+			mTwitter = TwitUtils.getTwitterInstance(this);
+			reloadTimeLine();
 		}
+	}
+	
+	private class TweetAdapter extends ArrayAdapter<String>{
+		
+		public TweetAdapter(Context context){
+			super(context, android.R.layout.simple_list_item_1);
+		}
+	}
+	
+	private void reloadTimeLine(){
+		AsyncTask<Void, Void, List<String>> task = new AsyncTask<Void, Void, List<String>>(){
+			
+			@Override
+			protected List<String> doInBackground(Void... params){
+				try{
+					ResponseList<twitter4j.Status> timeline = mTwitter.getHomeTimeline();
+					ArrayList<String> list = new ArrayList<String>();
+					for (twitter4j.Status status : timeline){
+						list.add(status.getText());
+					}
+					return list;
+				}catch (TwitterException e){
+					e.printStackTrace();
+				}
+				return null;
+			}
+			
+			@Override
+			protected void onPostExecute(List<String> result){
+				if(result != null){
+					mAdapter.clear();
+					for(String status : result){
+						mAdapter.add(status);
+					}
+					getListView().setSelection(0);
+				}else{
+					showToast("なにかがおかしいみたい");
+				}
+			}
+		};
+		task.execute();
+	}
+	
+	private void showToast(String text){
+		Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
 	}
 
 	@Override
